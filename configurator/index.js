426d5f09b2aa23da39a910f8cc5526e1d2f47c57
Adam Argyle <argyle@google.com>
2024-02-23 12:25:40 -0800

    config presentation updates

--- configurator/index.js
+++ configurator/index.js
@@ -4,22 +4,47 @@ navigationButtons.oninput = () => {
   const value = new FormData(carouselConfiguration).get('buttons')
   const buttons = customCarousel.querySelectorAll(':scope > button')
 
-  buttons.forEach(button => {
-    button.style.display = value === 'true' ? null : 'none'
-  })
+  function mutate() {
+    buttons.forEach(button => {
+      button.style.display = value === 'true' ? null : 'none'
+    })
+
+    buttonsOutput.textContent = value === 'false' ? `` : `&::scroll-left-button  { content: '<' }
+    &::scroll-right-button { content: '>' }`
+  }
+
+  if (document.startViewTransition)
+    document.startViewTransition(() => mutate())
+  else
+    mutate()
 }
 
 paginationButtons.oninput = () => {
   const value = new FormData(carouselConfiguration).get('pagination')
   const markers = customCarousel.querySelector(':scope > .grid-flow---markers')
 
-  markers.style.display = value === 'true' ? null : 'none'
+  function mutate() {
+    markers.style.display = value === 'true' ? null : 'none'
+    markersOutput.textContent = value === 'true' ? '&::scroll-markers { content: " " }' : ''
+  }
+
+  if (document.startViewTransition)
+    document.startViewTransition(() => mutate())
+  else
+    mutate()
 }
 
 visibleScrollbar.oninput = () => {
   const value = new FormData(carouselConfiguration).get('scrollbar')
+  const scroller = customCarousel.querySelector(':scope > .grid-flow---scroller')
 
-  customCarousel
-    .querySelector('.grid-flow---scroller')
-    .classList.toggle('scrollbar-visible', value === 'true')
+  function mutate() {
+    scroller.style.display = value === 'true' ? null : 'none'
+    scrollerOutput.textContent = value === 'true' ? '&::scrollbar { display: block }' : ''
+  }
+
+  if (document.startViewTransition)
+    document.startViewTransition(() => mutate())
+  else
+    mutate()
 }
